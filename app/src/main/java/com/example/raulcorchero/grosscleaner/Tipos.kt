713e48aa273fdbc653d8c0cfc5deb75da9535d1f
Alberto Baron <bertuko@hotmail.com>
2016-05-12 09:57:50 +0200

    Clases nuevas

---
+++ app/src/main/java/com/example/raulcorchero/grosscleaner/Tipos.kt
@@ -0,0 +1,9 @@
+package com.example.raulcorchero.grosscleaner
+
+/**
+ * Created by alberto.baron on 12/05/2016.
+ */
+enum class TipoSituacionFamiliar {
+
+
+}
---
+++ app/src/main/java/com/example/raulcorchero/grosscleaner/User.kt
@@ -0,0 +1,13 @@
+package com.example.raulcorchero.grosscleaner
+
+class User {
+
+    var name: String = ""
+    var GradoDiscapacidad: Int = 0
+    var NumDescendientes: Int = 0
+    var FuerzaRetencion: Boolean = false
+    var PorcentajeRetencion: Int = 0
+
+
+
+}
