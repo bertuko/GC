84203d00fc6b625bc9e018acb33de2c3d9483dfc
david.negrete <david.negrete@wolters.kluwer.com>
2016-05-12 15:30:30 +0200

    kotlin

--- app/src/main/java/com/example/raulcorchero/grosscleaner/Utilities.kt
+++ app/src/main/java/com/example/raulcorchero/grosscleaner/Utilities.kt
@@ -3,25 +3,32 @@ import android.content.*
 import java.io.*
 import com.fasterxml.jackson.module.kotlin.*
 
-class Utilities () {
-    public fun GetConfiguration () {
-//        val assetManager = getAssets()
-//        val oXML = assetManager.open(R.string.configurationFileName)
+class Utilities {
+    private var oContext: Context
+
+    constructor (c: Context) {
+        this.oContext = c
+    }
+
+    fun GetConfiguration () {
+        var oXML = oContext.resources.openRawResource (R.xml.tables_configuration)
+
+        var p = 0
 
     }
 
-    public fun saveUserdata(ctx: Context, usuario: User) {
+    public fun saveUserdata(usuario: User) {
         val filename: String = R.string.userDataFileName.toString()
-        val file: File = File(ctx.getFilesDir(), filename)
-        var outputStream: FileOutputStream = ctx.openFileOutput(filename, Context.MODE_PRIVATE);
+        val file: File = File(this.oContext.getFilesDir(), filename)
+        var outputStream: FileOutputStream = this.oContext.openFileOutput(filename, Context.MODE_PRIVATE);
         val mapper = jacksonObjectMapper()
         mapper.writeValue(outputStream, usuario)
         outputStream.close();
     }
 
-    public fun LoadUserdata(ctx: Context) : User {
+    public fun LoadUserdata() : User {
         val filename: String = R.string.userDataFileName.toString()
-        var fis: FileInputStream = ctx.openFileInput(filename)
+        var fis: FileInputStream = this.oContext.openFileInput(filename)
         var isr: InputStreamReader = InputStreamReader(fis)
         val mapper = jacksonObjectMapper()
         var usuario: User = mapper.readValue<User>(isr.readText())
