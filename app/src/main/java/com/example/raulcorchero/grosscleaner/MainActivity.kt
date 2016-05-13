8b6726a3ffd3e8c78fb5af13b2c351293d030c14
Alberto Baron <bertuko@hotmail.com>
2016-05-13 12:10:54 +0200

    asdf

--- app/src/main/java/com/example/raulcorchero/grosscleaner/MainActivity.kt
+++ app/src/main/java/com/example/raulcorchero/grosscleaner/MainActivity.kt
@@ -14,33 +14,50 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var u = Utilities(this.getBaseContext())
+        if (!u.ExistsUserdata()){
+            val intent = Intent(this@MainActivity, Setting::class.java)
+            startActivity(intent)
+        }
     }
 
     fun calcular (v: View){
         var u = Utilities(v.context)
         var usuario: User = u.LoadUserdata()
-        var sImporteBruto: String = this.ImporteBruto.getText().toString().trim()
-        var sNumPagas: String = this.NumPagas.getText().toString().trim()
 
-        if ( sImporteBruto != "") sImporteBruto = "0"
-        if ( sNumPagas != "" ) sNumPagas = "0"
+        //Recuperamos datos de la pantalla al usuario
+        usuario.ImporteBruto = initalizeValue(this.ImporteBruto.getText().toString()).toFloat()
+        usuario.NumPagas = initalizeValue(this.NumPagas.getText().toString()).toInt()
 
-        usuario.ImporteBruto = sImporteBruto.toFloat()
-        usuario.NumPagas = sImporteBruto.toInt()
         u.saveUserdata(usuario)
 
-        if ((usuario.ImporteBruto != 0f ) && (usuario.NumPagas != 0)){
+        if ((usuario.ImporteBruto != 0f ) || (usuario.NumPagas < 12)){
             // Llamar al calculo
             val c: Calculation = Calculation()
             c.Calculate()
         } else {
-            //mensaje de aviso... Hay que introducir importes o si no...
-            alert("Oye", "¿De que vas?") {
-                positiveButton("Yes") { }
-                negativeButton("No") { }
-            }.show()
+            var msg: String = ""
+            if (usuario.ImporteBruto != 0f ) {
+                msg = "Debe introducir un Salario Bruto"
+            } else if (usuario.NumPagas < 12){
+                msg = "El número de pagas debe ser superior a 12"
+            }
+            showAlert("Atención!", msg)
         }
 
+    }
+
+    fun initalizeValue (datastring: String): String{
+        var r: String = datastring.trim()
+        if (r == "") {r = "0"}
+        return r
+    }
+
+    fun showAlert (title: String, message: String){
+        alert(message, title) {
+            positiveButton("Aceptar") { }
+        }.show()
     }
 
     fun viewSettings(v: View){
